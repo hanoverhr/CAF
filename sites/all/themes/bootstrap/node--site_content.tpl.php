08195292aa854d7805017cdf006c003f32bdc874
Dorian Winterfeld <dorianw@verizon.net>
2015-01-31 14:56:22 -0500

    more work on node template.

--- sites/all/themes/bootstrap/node--site_content.tpl.php
+++ sites/all/themes/bootstrap/node--site_content.tpl.php
@@ -80,25 +80,85 @@
  * @ingroup themeable
  */
 ?>
-<?php //print render($content['field_content_designator']); ?>
-
-<?php //switch ($content['field_content_designator']) { 
-	//case 'Announcement':
+<?php //print($content['field_content_designator'][0]['#markup']); 
+$designator = $content['field_content_designator'][0]['#markup'];
 ?>
 
+<?php switch ($designator) { 
+		case 'Announcement':
+?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 <div class="content"<?php print $content_attributes; ?>>
-<div class="modal-dialog">
-<h1><?php print render($title) ?><?php print render($content['field_image']) ?>
+<div class="modal-body">
+<h1><?php print $title ?>
+<?php if (isset($content['field_image'])) {?>
+<?php print render($content['field_image']) ?>
+<?php }; ?>
+</h1>
 <p></p>
-<p><?php print render($content['body']) ?><br>
+<p><?php print $content['body'][0]['#markup'] ?></p>
 <br>
 <br>
 </p>
 </div>
- </div>
 </div>
+</div>
+<?php      break; ?>  
 
-<?php //switch 'Services' ?>
+<?php 		case 'About Us': ?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div class="content"<?php print $content_attributes; ?>>
+<div class="modal-body">
+<h3><?php print $title ?></h3>
+<?php if (isset($content['field_image'])) {?>
+<?php print render($content['field_image']) ?>
+<?php }; ?>
+<p><?php print $content['body'][0]['#markup'] ?></p>
+</div>
+</div>
+</div>
+<?php      break; ?>   
+ 
+<?php 		case 'Careers': ?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div class="content"<?php print $content_attributes; ?>>
+<div class="modal-body">
+<h2><?php print $title ?></h2>
+<?php if (isset($content['field_image'])) {?>
+<?php print render($content['field_image']) ?>
+<?php }; ?>
+<p><?php print $content['body'][0]['#markup'] ?></p>
+</div>
+</div>
+</div>
+<?php      break; ?>    
 
-<?php //}; ?>
+<?php 		case 'FAQ': ?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div class="content"<?php print $content_attributes; ?>>
+<div class="modal-body">
+<h2><?php print $title ?></h2>
+<?php if (isset($content['field_image'])) {?>
+<?php print render($content['field_image']) ?>
+<?php }; ?>
+<p><?php print $content['body'][0]['#markup'] ?></p>
+</div>
+</div>
+</div>
+</div>
+<?php      break; ?>  
+
+<?php 		case 'Site Information': ?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div class="content"<?php print $content_attributes; ?>>
+<div class="modal-body">
+<h2><?php print $title ?></h2>
+<?php if (isset($content['field_image'])) {?>
+<?php print render($content['field_image']) ?>
+<?php }; ?>
+<p><?php print $content['body'][0]['#markup'] ?></p>
+</div>
+</div>
+</div>
+<?php      break; ?>  
+<?php }; ?>
